95a871d15b68a14660bbe1bb834f9723e311a3d2
ZaneerahEssop <2655572@students.wits.ac.za>
2025-09-04 08:27:40 +0200

    frest start

--- Backend/src/Routes/newEvent.routes.js
+++
@@ -1,8 +0,0 @@
-// Backend/src/Routes/newEventRoutes.js
-const express = require("express");
-const router = express.Router();
-const { createEvent } = require("../Controllers/newEvent.controller"); // controller function
-
-router.post("/", createEvent); // <--- pass the function, not the module
-
-module.exports = router; // <-- export the router
