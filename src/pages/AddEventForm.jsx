7049f94da68095692bc33955e0d85048742ccdf2
TariroMuvevi <2667536@students.wits.ac.za>
2025-09-09 17:45:29 +0200

    Remove unused import FaTrash from AddEventForm

--- src/pages/AddEventForm.jsx
+++ src/pages/AddEventForm.jsx
@@ -3,7 +3,7 @@ import "../AddEventForm.css";
 import React, { useState, useEffect } from "react";
 import { supabase } from '../client';
 import { useNavigate } from "react-router-dom";
-import { FaCalendarAlt, FaClock, FaUsers, FaTrash, FaUpload, FaSearch, FaTimes, FaFileAlt, FaPlus, FaArrowLeft } from "react-icons/fa";
+import { FaCalendarAlt, FaClock, FaUsers, FaUpload, FaSearch, FaTimes, FaFileAlt, FaPlus, FaArrowLeft } from "react-icons/fa";
 
 export default function AddEventForm() {
   const navigate = useNavigate();
@@ -145,7 +145,7 @@ export default function AddEventForm() {
       if (error) throw error;
 
       alert("Event created successfully!");
-      navigate("/planner-dashboard");
+      navigate("/dashboard");
 
     } catch (error) {
       console.error("Error creating event:", error);
