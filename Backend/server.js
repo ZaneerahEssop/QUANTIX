cfc0848d07d4a026f3929909b3372e3b6eaba8ed
ammaarahcassim <2453749@students.wits.ac.za>
2025-09-12 11:16:36 +0200

    server.js updates

--- Backend/server.js
+++ Backend/server.js
@@ -35,3 +35,9 @@ app.get("/users", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
