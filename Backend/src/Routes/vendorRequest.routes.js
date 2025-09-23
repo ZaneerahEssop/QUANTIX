818344fc5559b2d338d934469d6f31165471e2c1
ammaarahcassim <2453749@students.wits.ac.za>
2025-09-23 10:45:54 +0200

    vendor requests under vendor management

--- Backend/src/Routes/vendorRequest.routes.js
+++ Backend/src/Routes/vendorRequest.routes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const {
   createVendorRequest,
   getVendorRequestByVendorId,
+  getVendorRequestByEventId,
   updateVendorRequest,
 } = require("../Controllers/vendorRequest.controller");
 
 const router = express.Router();
 
 router.post("/", createVendorRequest);
+
+router.get("/event/:event_id", getVendorRequestByEventId);
 
 router.get("/:vendor_id", getVendorRequestByVendorId);
 
